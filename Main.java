a0dbecc18c18306aa980b8902ac594b630f3dee9
Benazzz <178609468+Benazzz@users.noreply.github.com>
2025-02-08 19:41:25 +0200

    Completed Decompress method

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Compress("abc"));
+        //System.out.println(Compress("abc"));
+        System.out.println(Decompress("a10b2"));
     }
 
     public static String Compress(String text) {
@@ -23,12 +24,25 @@ public class Main {
                 lastChar = text.charAt(i);
             }
         }
+
         newString += lastChar + Integer.toString(numOfReps);
         return newString;
     }
 
     public static String Decompress(String text) {
+
+        if (text == null || text.isEmpty()) {
+            return "";
+        }
+
         String newString = "";
+
+        for(int i = 1; i < text.length(); i += 2) {
+            int intValue = Integer.parseInt(String.valueOf(text.charAt(i)));
+            String currentChar = String.valueOf(text.charAt(i - 1));
+            newString += currentChar.repeat(intValue);
+        }
+
         return newString;
     }
 }
